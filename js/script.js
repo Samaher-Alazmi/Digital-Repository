c8bafdd35c466db6f9d208edfcf443ebcdeb9574
Samaher3 <120955636+Samaher3@users.noreply.github.com>
2023-09-05 10:06:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+console.clear();
+
+//const loginBtn = document.getElementById('login');
+//const signupBtn = document.getElementById('signup');
+
+//loginBtn.addEventListener('click', (e) => {
+//	let parent = e.target.parentNode.parentNode;
+//	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+//		if (element !== "slide-up") {
+//			parent.classList.add('slide-up')
+//		} else {
+//			signupBtn.parentNode.classList.add('slide-up')
+//			parent.classList.remove('slide-up')
+//		}
+//	});
+//});
+
+//signupBtn.addEventListener('click', (e) => {
+//	let parent = e.target.parentNode;
+//	Array.from(e.target.parentNode.classList).find((element) => {
+//		if (element !== "slide-up") {
+//			parent.classList.add('slide-up')
+//		} else {
+//			loginBtn.parentNode.parentNode.classList.add('slide-up')
+//			parent.classList.remove('slide-up')
+//		}
+//	});
+//});
+
+
+function myFunction() {
+	var checkBox = document.getElementById("myCheck");
+	var text = document.getElementById("text");
+	if (checkBox.checked == true) {
+		CV.style.display = "block";
+		Exp.style.display = "block";
+	} else {
+		CV.style.display = "none";
+		Exp.style.display = "none";
+	}
+}
